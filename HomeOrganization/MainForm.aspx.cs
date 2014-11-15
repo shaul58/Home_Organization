28e14b2e00c85cbe68b644fcd2eb00fcb675b784
shaul vachnun <shaul58@gmail.com>
2014-11-15 21:23:50 +0200

    test the deployment project

--- HomeOrganization/MainForm.aspx.cs
+++ HomeOrganization/MainForm.aspx.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Windows;
+using System.Threading;
 
 namespace HomeOrganization
 {
@@ -18,6 +19,10 @@ namespace HomeOrganization
         protected void Button1_Click(object sender, EventArgs e)
         {
             Button1.Text = "avital HAVA";
+            Thread.Sleep(3000);
+
+            Response.Redirect("messagesPage");
+            
 
         }
     }
